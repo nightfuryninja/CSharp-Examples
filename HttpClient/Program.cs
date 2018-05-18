506ac885732f812f7fedb11ae596e7e064a2aa47
nightfuryninja <nightfurryninja@gmail.com>
2018-05-19 00:47:19 +0100

    Add files via upload

---
+++ HttpClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Test_Branch
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Our Instance of W
+        /// </summary>
+        static HttpClient Client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            PostGoogle().GetAwaiter().GetResult();
+        }
+
+        static async Task GetGoogle()
+        {
+            string URL = "https://google.com";
+            string CallBack = await Client.GetStringAsync(URL);
+            Console.WriteLine(CallBack);
+        }
+
+        static async Task PostGoogle()
+        {
+            string URL = "http://ptsv2.com/t/1ustz-1526669968/post";
+            string Data = @"{
+                              'name': 'morpheus',
+                              'job': 'leader'
+                           }";
+            HttpContent PostData = new StringContent(Data, Encoding.UTF8, "application/json");
+            HttpResponseMessage Response = await Client.PostAsync(URL, PostData);
+            string Callback = Response.IsSuccessStatusCode.ToString();
+            Console.WriteLine(Callback);
+
+        }
+
+    }
+}
